ed47b0bf85b3df655eef38ea41ec48bb87dfedd9
digitalt <cmerchant9@comcast.net>
2019-08-12 08:25:26 -0400

    Add files via upload
    Initial conversion to Visual Studio.

---
+++ tuckfirtle/MiningMode.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (C) 2019, The Tuckfirtle Developers
+// 
+// Please see the included LICENSE file for more information.
+
+namespace Tuckfirtle.Miner.Config.Model
+{
+    internal enum MiningMode
+    {
+        Test = 0
+    }
+}
